21fd9843ef59bec15d23caf1f7af73189429b20e
Danny Vega <67448064+dannyvega-studio@users.noreply.github.com>
2021-04-11 14:17:51 -0600

    Updates

---
+++ precache-manifest.a8d8e94fa2f4c42b997da4b9c5fdc489.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a7397724dbee1ac5e1b2bf9d850934a",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "0120fbd82d5e10a396fe",
+    "url": "/pomodoro-clock/static/css/main.3c5926fd.chunk.css"
+  },
+  {
+    "revision": "3c3aa9dd0ebc03d88da4",
+    "url": "/pomodoro-clock/static/js/2.3e63e5c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.3e63e5c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0120fbd82d5e10a396fe",
+    "url": "/pomodoro-clock/static/js/main.97d68c55.chunk.js"
+  },
+  {
+    "revision": "a4dcdb46a55158e82e38",
+    "url": "/pomodoro-clock/static/js/runtime-main.6695a290.js"
+  }
+]);
